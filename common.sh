fae11aec66939bacd4431b08a0398e8298c68b5d
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-05-28 18:01:59 +0530

    auto script

--- common.sh
+++ common.sh
@@ -87,7 +87,7 @@ setup_app(){
         mkdir -p /app 
         VALIDATE $? "Creating app directory "
 
-        curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+        curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip 
         VALIDATE $? "Downloading $app_name zip to tmp dir "
 
         rm -rf /app/*
